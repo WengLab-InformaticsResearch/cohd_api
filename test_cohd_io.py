ada9392a7a7aea393430e0c1e19a5c09c927248c
Casey Ta <casey.ta@gmail.com>
2020-01-27 22:24:37 -0800

    Update cohd-monitor.yml to run every 12 hours and on push. Add a test case that should fail to make sure the test itself is working.

--- test_cohd_io.py
+++ test_cohd_io.py
@@ -3,3 +3,6 @@ from notebooks.cohd_requests import *
 # Check that
 df_dataset = datasets()
 assert df_dataset is not None and df_dataset.shape[0] >= 3
+
+# Quick test to make sure that failures are properly reported
+assert 1 == 2
