f5976bd3ddd3348f4c2e17f7fbcd9c376eb61b05
Casey Ta <casey.ta@gmail.com>
2020-01-29 00:04:35 -0800

    Basic framework for testing harness

--- cohd/test_unit_tests.py
+++ cohd/test_unit_tests.py
@@ -1,6 +1,8 @@
 import cohd_utilities
 
-def test_cohd_utilties():
+def test_cohd_double_poisson_ci():
     assert cohd_utilities.double_poisson_ci(50) == (29.0, 75.0)
 
+
+def test_ln_ratio_ci():
     assert cohd_utilities.ln_ratio_ci(50, 2.0) == (1.4552728245583282, 2.4054651081081646)
