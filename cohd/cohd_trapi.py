003bccbec9b31a061a3db1278977fb939722232b
Casey Ta <casey.ta@gmail.com>
2021-01-21 12:47:46 -0800

    Add SRI Node Normalizer into process for mapping bewteen BLM and OMOP

--- cohd/cohd_trapi.py
+++ cohd/cohd_trapi.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import requests
+from requests.compat import urljoin
+from typing import Union, Any, Iterable, Dict
 
 from .cohd_utilities import ln_ratio_ci, ci_significance
 from .omop_xref import ConceptMapper
@@ -409,31 +412,48 @@ class BiolinkConceptMapper:
 
         self._oxo_concept_mapper = ConceptMapper(oxo_mappings, self.distance, self.local_oxo)
 
-    def map_to_omop(self, curie):
+    def map_to_omop(self, curies: Iterable[str]) -> Dict[str, Any]:
         """ Map to OMOP concept from ontology
 
         Parameters
         ----------
-        curie: CURIE
+        curies: list of CURIEs
 
         Returns
         -------
         Dict like:
-        {
+        {curie: {
             "omop_concept_name": "Osteoarthritis",
             "omop_concept_id": 80180,
             "distance": 2
+            }
         }
         or None
         """
-        # Convert from Biolink Model prefix to OxO prefix. If the prefix isn't in the mappings between BLM and OxO, try
-        # the mapping with the original prefix
-        oxo_curie = BiolinkConceptMapper.map_blm_prefixes_to_oxo_prefixes(curie)
+        # Get equivalent identifiers from SRI Node Normalizer
+        normalized_nodes = SriNodeNormalizer.get_normalized_nodes(curies)
 
-        # Get mappings from ConceptMapper
-        return self._oxo_concept_mapper.map_to_omop(oxo_curie)
+        omop_mappings = dict()
+        for curie in curies:
+            # First, try mapping via OxO on the provided CURIE
+            oxo_curie = BiolinkConceptMapper.map_blm_prefixes_to_oxo_prefixes(curie)
+            omop_mapping = self._oxo_concept_mapper.map_to_omop(oxo_curie)
 
-    def map_from_omop(self, concept_id, domain_id=None):
+            if omop_mapping is None and normalized_nodes is not None and curie in normalized_nodes and \
+                    normalized_nodes[curie] is not None:
+                # Try OxO on each of the equivalent identifiers from SRI Node Normalizer
+                equivalent_ids = normalized_nodes[curie]['equivalent_identifiers']
+                for identifier in equivalent_ids:
+                    oxo_curie = BiolinkConceptMapper.map_blm_prefixes_to_oxo_prefixes(identifier['identifier'])
+                    omop_mapping = self._oxo_concept_mapper.map_to_omop(oxo_curie)
+                    if omop_mapping is not None:
+                        break
+
+            omop_mappings[curie] = omop_mapping
+
+        return omop_mappings
+
+    def map_from_omop(self, concept_id, domain_id):
         """ Map from OMOP concept to appropriate domain-specific ontology.
 
         Parameters
@@ -454,10 +474,118 @@ class BiolinkConceptMapper:
         # Get mappings from ConceptMapper
         mappings = self._oxo_concept_mapper.map_from_omop(concept_id, domain_id)
 
+        if mappings is None:
+            return mappings
+
         # For each of the mappings, change the prefix to the Biolink Model convention
         for mapping in mappings:
             # Convert from OxO prefix to BLM prefix. If the prefix isn't in the mappings between BLM and OxO, keep the
             # OxO prefix
             mapping['target_curie'] = BiolinkConceptMapper.map_oxo_prefixes_to_blm_prefixes(mapping['target_curie'])
 
-        return mappings
+        # Get the canonical BLM ID for each curie from the OxO mapping
+        curies = [x['target_curie'] for x in mappings]
+        normalized_nodes = SriNodeNormalizer.get_normalized_nodes(curies)
+
+        # # Change any mappings from OxO to the canonical IDs
+        # # Note 1: don't increment the distance since these are considered equivalent identifiers
+        # # Note 2: Canoncalization may yield multiple rows of mappings to the same canonical ID but with different
+        # # distances. Only keep the mapping with the shortest distance
+        # mappings_dict = dict()
+        # for mapping in mappings:
+        #     target_curie = mapping['target_curie']
+        #     if target_curie in normalized_nodes and normalized_nodes[target_curie] is not None:
+        #         if target_curie in mappings_dict:
+        #             # Already seen this canonical ID. Retain the shortest distance for a given canonical ID
+        #             if mapping['distance'] < mappings_dict[target_curie]['distance']:
+        #                 mappings_dict[target_curie]['distance'] = mapping['distance']
+        #         else:
+        #             canonical_node = normalized_nodes[target_curie]['id']
+        #             mapping['target_curie'] = canonical_node['identifier']
+        #             if 'label' in canonical_node:
+        #                 mapping['target_label'] = canonical_node['label']
+        #             mappings_dict[target_curie] = mapping
+        #     else:
+        #         # No normalized node found. Use the original mapping
+        #         mappings_dict[target_curie] = mapping
+
+        # Find the mapping with the shortest distance canonical mapping
+        if normalized_nodes is not None:
+            for d in range(self.distance + 1):
+                # Get all mappings with the current distance
+                m_d = [m for m in mappings if m['distance'] == d]
+                for m in m_d:
+                    cm_target_curie = m['target_curie']
+                    if cm_target_curie in normalized_nodes and normalized_nodes[cm_target_curie] is not None:
+                        canonical_node = normalized_nodes[cm_target_curie]['id']
+                        m['target_curie'] = canonical_node['identifier']
+                        if 'label' in canonical_node:
+                            m['target_label'] = canonical_node['label']
+                        return m
+
+        # Did not find any canonical nodes from SRI Node Normalizer
+        # Choose one of the mappings to be the main identifier for the node. Prioritize distance first, and then
+        # choose by the order of prefixes listed in the Concept Mapper.
+        blm_category = map_omop_domain_to_blm_class(domain_id)
+        blm_prefixes = self.biolink_mappings.get(blm_category, [])
+        for d in range(self.distance + 1):
+            # Get all mappings with the current distance
+            m_d = [m for m in mappings if m['distance'] == d]
+
+            # Look for the first matching prefix in the list of biolink prefixes
+            for prefix in blm_prefixes:
+                for m in m_d:
+                    if m['target_curie'].split(':')[0] == prefix:
+                        # Found the priority prefix with the shortest distance. Return the mapping
+                        return m
+
+        return None
+
+
+class SriNodeNormalizer:
+    base_url = 'https://nodenormalization-sri.renci.org'
+    endpoint_get_normalized_nodes = 'get_normalized_nodes'
+
+    @staticmethod
+    def get_normalized_nodes(curies: Iterable[str]) -> Union[Dict[str, Any], None]:
+        """ Straightforward call to get_normalized_nodes. Returns json from response.
+
+        Parameters
+        ----------
+        curies - list of curies
+
+        Returns
+        -------
+        JSON response from endpoint or None. Each input curie will be a key in the response. If no normalized node is
+        found, the entry will be null.
+        """
+        url = urljoin(SriNodeNormalizer.base_url, SriNodeNormalizer.endpoint_get_normalized_nodes)
+        response = requests.post(url=url, json={'curies': curies})
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    @staticmethod
+    def get_canonical_identifiers(curies: Iterable[str]) -> Union[Dict[str, Union[str, None]], None]:
+        """ Retrieve the canonical identifier
+
+        Parameters
+        ----------
+        curies - list of CURIES
+
+        Returns
+        -------
+        dict of canonical identifiers for each curie. If curie not found, then None
+        """
+        j = SriNodeNormalizer.get_normalized_nodes(curies)
+        if j is None:
+            return None
+
+        canonical = dict()
+        for curie in curies:
+            if curie in j and j[curie] is not None:
+                canonical[curie] = j[curie]['id']
+            else:
+                canonical[curie] = None
+        return canonical
