9783a5232a60672271cb440f1c31df95ab8b9b7e
caseyta <casey.ta@gmail.com>
2024-03-12 23:04:00 +0000

    Update to Biolink 4.1.0

--- cohd/translator/__init__.py
+++ cohd/translator/__init__.py
@@ -2,5 +2,5 @@ from bmt import Toolkit
 
 
 # Static instance of the Biolink Model Toolkit
-bm_version = 'v3.5.0'
+bm_version = 'v4.1.0'
 bm_toolkit = Toolkit(f'https://raw.githubusercontent.com/biolink/biolink-model/{bm_version}/biolink-model.yaml')
