85f7220e1be425f52f0b9c584e3b6fee6241626e
caseyta <casey.ta@gmail.com>
2024-04-12 22:36:51 +0000

    Update reasoner-validator version requirement. Update biolink model version

--- cohd/translator/__init__.py
+++ cohd/translator/__init__.py
@@ -2,5 +2,5 @@ from bmt import Toolkit
 
 
 # Static instance of the Biolink Model Toolkit
-bm_version = 'v4.1.0'
+bm_version = 'v4.1.6'
 bm_toolkit = Toolkit(f'https://raw.githubusercontent.com/biolink/biolink-model/{bm_version}/biolink-model.yaml')
